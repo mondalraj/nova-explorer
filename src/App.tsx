947a4885d1a489ab3130880b00e8fb263043dce6
Rajib Mondal <mondal021002rajib@gmail.com>
2022-10-23 20:50:41 +0530

    project setup

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="text-blue-600 font-bold underline">
+      Vite - React TypeScript Starter
+    </div>
+  );
+}
+
+export default App;
